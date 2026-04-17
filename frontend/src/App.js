10f348e90309b01840ceadf7e6537e4d36514bb7
Preome <tasdidanpreome@gmail.com>
2026-04-18 01:30:38 +0600

    fix again

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './components/Login';
 import Signup from './components/Signup';
 import TeacherDashboard from './components/TeacherDashboard';
